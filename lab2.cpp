423854891d9a537d29841ae661c8afe44f143466
xQbsonKEKW <jakubszczuka@gmail.com>
2020-11-10 17:44:29 +0000

    resourceManager class added

--- lab2.cpp
+++ lab2.cpp
@@ -1,6 +1,49 @@
 #include <iostream>
+using namespace std;
+
+class Wektor
+{
+
+public:
+    Wektor(int len)
+    {
+        this->wektor = new double[len];
+        for (int i = 0; i < len; i++)
+            wektor[i] *= 0.;
+        this->dlugosc = len;
+    }
+    ~Wektor() { delete[] this->wektor; }
+
+    void print()
+    {
+        for (int i = 0; i < this->dlugosc; i++)
+            cout << wektor[i] << ' ';
+    }
+    void zmienDlugosc(int jup)
+    {
+        if (jup <= this->pojemnosc) {
+            this->dlugosc = jup;
+            for (int i = jup; i < this->pojemnosc; i++)
+                wektor[i] *= 0.;
+        }
+        else {
+            ; //TODO: dokończ zadanie 6
+        }
+    }
+
+    int getDlugosc() { return this->dlugosc; }
+    int getPojemnosc() { return this->pojemnosc; }
+
+    double* wektor;
+
+private:
+    int dlugosc;
+    int pojemnosc;
+    int 
+};
 
 int main()
 {
-    puts("Hello, World!");
+    Wektor d = Wektor(10);
+    d.print();
 }
--- zal/ResourceManager.hpp
+++ zal/ResourceManager.hpp
@@ -1,8 +1,75 @@
 #pragma once
 
 #include "Resource.hpp"
+#include <iostream>
+
+using namespace std;
+
 
 class ResourceManager
 {
-    // Twoja implementacja tutaj
+public:
+
+  ResourceManager() {
+    res = new Resource;
+  }
+
+    ResourceManager(const ResourceManager& rs) // copying constructor
+    {
+        cout << "copying_constr" << endl;
+        res = new Resource{*rs.res};
+        cout << res << endl;
+    }
+
+    ResourceManager& operator=(const ResourceManager& rs)
+    {
+        if (!(res == nullptr)) {
+            cout << "selfDeleting" << endl;
+            delete res;
+        }
+        cout << "copying_operator" << endl;
+        res = new Resource{*rs.res};
+        cout << res << endl;
+        return *this;
+    }
+
+    ResourceManager(ResourceManager&& rs) // moving constructor
+    {
+        cout << "moving_constr" << endl;
+        res    = move(rs.res); // wywola konstruktor przenoszacy
+        rs.res = nullptr;
+    }
+
+    ResourceManager& operator=(ResourceManager&& rs)
+    {
+        if (&rs == this) // prevent self-copying
+        {
+            cout << "selfMoving!!!!!!!!!!!!!!!!!!!!!!!" << endl;
+            return *this;
+        }
+        else if (!(res == nullptr)) {
+            cout << "selfDeleting" << endl;
+            delete res;
+        }
+        cout << "moving_operator" << endl;
+        res = move(rs.res); // wywola konstruktor przenoszacy
+
+        rs.res = nullptr;
+        return *this;
+    }
+
+    ~ResourceManager()
+    {
+        cout << "deconst" << endl;
+        cout << res << endl;
+        delete res;
+    }
+
+  double get()
+  {
+    return res->get();
+  }
+
+private:
+    Resource* res = nullptr;
 };
